d165eab139490547da4f7288e45d7ec5f487bb29
biomaxima <boxmaxima@outlook.com>
2023-12-26 15:57:45 +0100

    N gram model completed

--- n-gram.py
+++ n-gram.py
@@ -1,8 +1,8 @@
 import nltk
 from nltk.tokenize import word_tokenize
-# nltk.download('punkt')
 
-def test_fun(tokens_set1,tokens_set2):
+
+def test_fun(tokens_set1, tokens_set2):
     """
     Tests the split_tokens function with various assertions.
 
@@ -13,18 +13,18 @@ def test_fun(tokens_set1,tokens_set2):
 
     # Tests for tokens_set1 with n=3
     assert len(split_tokens(tokens_set1, n=3)) == 21
-    assert split_tokens(tokens_set1,3)[0] == ('<BOS>', '<BOS>', 'It')
-    assert split_tokens(tokens_set1, n=3)[10] == ('we', 'are', 'dealing')
+    assert split_tokens(tokens_set1, 3)[0] == ("<BOS>", "<BOS>", "It")
+    assert split_tokens(tokens_set1, n=3)[10] == ("we", "are", "dealing")
 
     # Tests for tokens_set1 with n=2
     assert len(split_tokens(tokens_set1, n=2)) == 20
-    assert split_tokens(tokens_set1, n=2)[0] == ('<BOS>', 'It')
-    assert split_tokens(tokens_set1, n=2)[10] == ('are', 'dealing')
+    assert split_tokens(tokens_set1, n=2)[0] == ("<BOS>", "It")
+    assert split_tokens(tokens_set1, n=2)[10] == ("are", "dealing")
 
     # Tests for tokens_set2 with n=2
     assert len(split_tokens(tokens_set2, n=2)) == 10
-    assert split_tokens(tokens_set2, n=2)[0] == ('<BOS>', 'How')
-    assert split_tokens(tokens_set2, n=2)[9] == ('?', '<EOS>')
+    assert split_tokens(tokens_set2, n=2)[0] == ("<BOS>", "How")
+    assert split_tokens(tokens_set2, n=2)[9] == ("?", "<EOS>")
 
 
 def tokenizer(phrase):
@@ -38,7 +38,7 @@ def tokenizer(phrase):
     - tokens: A list of tokens extracted from the input phrase.
     """
     tokens = word_tokenize(phrase)
-    
+
     return tokens
 
 
@@ -60,7 +60,6 @@ def split_tokens(tokens, n=2):
     # Adding beginning and end tokens to the input tokens
     tokens = bos_list + tokens + eos_list
 
-
     k = len(tokens)
     split_tokens = []
 
@@ -68,40 +67,100 @@ def split_tokens(tokens, n=2):
         print("Tokens too small for n value splits")
     elif n < k:
         # Generate n-grams from the tokens
-        for i in range(k-n+1):
-            j = i+n
-            
+        for i in range(k - n + 1):
+            j = i + n
+
             split_tokens.append(tuple(tokens[i:j]))
 
     elif n == k:
         print("Same size as tokens")
 
-    
-    return(split_tokens)
+    return split_tokens
+
+
+def get_vocabulary_size(corpus):
+    # Get unique words by converting the corpus to a set
+    unique_words = set(corpus)
+
+    # Return the size of the unique words set
+    return len(unique_words)
+
+
+def build_ngram_model(sentence_tokens_list, n, smoothing="empirical", V=0):
+    """
+    Build an n-gram language model based on the given sentence tokens.
+
+    Args:
+    - sentence_tokens_list (list): List of tokenized sentences.
+    - n (int): The order of the n-gram model.
+    - smoothing (str): The type of smoothing to be applied. Default is 'empirical'.
+    - V (int): Vocabulary size used in Laplace smoothing. Default is 0.
+
+    Returns:
+    - dict: A dictionary representing the n-gram model with probabilities.
+
+    """
+    # Initialize an empty dictionary to store probabilities
+    probabilities_dict = {}
+
+    # Initialize an empty list to store n-grams
+    ngrams_lists = []
+
+    # Generate n-grams for each sentence in the list of tokens
+    for sentence in sentence_tokens_list:
+        ngrams = split_tokens(sentence, n)
+        ngrams_lists.append(ngrams)
+
+    # Flatten the list of n-grams
+    ngrams_list = [ngram_item for ngram_list in ngrams_lists for ngram_item in ngram_list]
+
+    # Populate dictionary with keys
+    for ngram in ngrams_list:
+        n_min_1_gram = ngram[0 : n - 1]
+        following_word = ngram[-1]
+        if n_min_1_gram in probabilities_dict:
+            probabilities_dict[n_min_1_gram].append(following_word)
+        else:
+            probabilities_dict[n_min_1_gram] = [following_word]
+
+    # Apply smoothing based on the specified method
+    if smoothing == "empirical":
+        for key in probabilities_dict.keys():
+            word_list = probabilities_dict[key]
+            my_dict = {w: word_list.count(w) / len(word_list) for w in word_list}
+            probabilities_dict[key] = my_dict
+
+    elif smoothing == "laplace":
+        for key in probabilities_dict.keys():
+            word_list = probabilities_dict[key]
+            # Calculate Laplace smoothed probabilities
+            my_dict = {w: (word_list.count(w) + 1) / (len(word_list) + V) for w in word_list}
+            probabilities_dict[key] = my_dict
+
+    return probabilities_dict
 
 
 def main():
     """
-    Main function to test tokenization and n-gram splitting.
+    Main function to create the model and apply different smoothing techniques.
     """
 
-    # Sample portuguese sentences (commented out)
-    # sentence1 = "Eu vou pousar a mão no teu quadril"
-    # sentence2 = "Multiplicar-te os pés por muitos mil"
-    
-    # Sample English sentences
-    sentence1 = "It shows, my dear Watson, that we are dealing with an exceptionally astude and dangerous man."
-    sentence2 = "How would Lausanne do, my dear Watson?"
+    # Create tokens list per sentence
+    sentence_list = []
+    with open("example_corpus.txt", "rt") as file:
+        for line in file:
+            sentence_list.append(list(line.split()))
 
-    # Tokenizing sentences
-    tokens1 = tokenizer(sentence1)
-    tokens2 = tokenizer(sentence2)
+    corpus = [word for words in sentence_list for word in words]
 
-    # Testing tokenization and n-gram splitting
-    test_fun(tokens1,tokens2)
+    # Get the vocabulary size of the corpus
+    vocabulary_size = get_vocabulary_size(corpus)
+
+    # Build empirical n-gram model
+    model_empirical = build_ngram_model(sentence_list, n=3)
+
+    # Build n-gram model with Laplace smoothing
+    model_laplace = build_ngram_model(sentence_list, n=3, smoothing="laplace", V=vocabulary_size)
 
 
 main()
-
-
-
